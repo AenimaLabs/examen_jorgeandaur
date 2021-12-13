430c4e4f2e25fbddf30859cb1c6cd7abfb03f9ab
JorgeAndaur <aenimalabs@gmail.com>
2021-12-13 00:29:35 -0300

    04_tabla departamento

---
+++ db/migrate/20211213032850_create_departamentos.rb
@@ -0,0 +1,9 @@
+class CreateDepartamentos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :departamentos do |t|
+      t.references :edificio, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
