cf9111935e157d86154fff2a3ddc3e90928bc27e
JorgeAndaur <aenimalabs@gmail.com>
2021-12-13 01:49:16 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+    Edificio.create(nombre_edificio: Faker::Company.name, direccion: Faker::Address.street_address, ciudad: Faker::Address.city)    
+end
+
+10.times do |i|
+    Departamento.create(numero_departamento: Faker::Number.number(digits: 3), edificio: Edificio.all.sample)
+end
