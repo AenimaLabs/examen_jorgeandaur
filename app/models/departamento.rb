cfcf6501d5133b4da1b88f040ca934af9243c359
JorgeAndaur <aenimalabs@gmail.com>
2021-12-13 02:26:21 -0300

    validaciones

--- app/models/departamento.rb
+++ app/models/departamento.rb
@@ -1,3 +1,22 @@
 class Departamento < ApplicationRecord
   belongs_to :edificio
+
+  validates :numero_departamento, presence: true , uniqueness: { case_sensitive: false}
+    
+  after_create :creacion_exitosa
+  
+  after_update :actualizacion_exitosa
+
+  private
+ 
+  
+  def creacion_exitosa
+      puts "Departamento creado con éxito"
+  end
+  
+  def actualizacion_exitosa
+      puts "Departamento actualizado con éxito"
+  end
+  
 end
+
--- app/models/edificio.rb
+++ app/models/edificio.rb
@@ -1,3 +1,23 @@
 class Edificio < ApplicationRecord
-    has_many :departamentos
+    has_many :departamentos, dependent: :destroy
+
+    validates :nombre_edificio, :direccion, :ciudad, presence: true , uniqueness: { case_sensitive: false}
+    
+  after_create :creacion_exitosa
+  
+  after_update :actualizacion_exitosa
+
+  private
+ 
+  
+  def creacion_exitosa
+      puts "Edificio creado con éxito"
+  end
+  
+  def actualizacion_exitosa
+      puts "Edificio actualizado con éxito"
+  end
+
+
+    
 end
