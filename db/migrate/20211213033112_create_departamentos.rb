589354699536f27a8ca585d66e35b81f0f0973fd
JorgeAndaur <aenimalabs@gmail.com>
2021-12-13 00:31:45 -0300

    05_correcion tabla departamento

--- db/migrate/20211213033112_create_departamentos.rb
+++ db/migrate/20211213033112_create_departamentos.rb
@@ -1,6 +1,7 @@
 class CreateDepartamentos < ActiveRecord::Migration[6.1]
   def change
     create_table :departamentos do |t|
+      t.integer :numero_departamento
       t.references :edificio, null: false, foreign_key: true
 
       t.timestamps
